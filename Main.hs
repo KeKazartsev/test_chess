157e7aeab601dfdd42187870c86ceda05a1d3826
Konstantin Kazartsev <kekazartsev@gmail.com>
2020-01-07 21:15:27 +0300

    haskell : improve performance : short_list ++ logn_list instead
    Signed-off-by: Konstantin Kazartsev <kekazartsev@gmail.com>

--- Main.hs
+++ Main.hs
@@ -6,13 +6,13 @@ test_hitten i x = (fst i /= fst x) && (snd i /= snd x) && (fst i - fst x /= snd 
 find1 :: (Ord(a), Num(a)) => [(a, a)] -> [(a, a)] -> [[(a, a)]]
 find1 res_start [] = [res_start]
 find1 res_start (d:[]) = [res_start ++ [d]]
-find1 res_start (d:deck) = find1 res_start deck ++ find1 (res_start ++ [d]) (filter (test_hitten d) deck)
+find1 res_start (d:deck) = find1 (res_start ++ [d]) (filter (test_hitten d) deck) ++ find1 res_start deck
 
 count = 11
+deck = [(x, y) | x <- [1..count], y <- [1..count]]
+res = filter (\x -> length x >= count) (find1 [] deck)
 
 main :: IO ()
 main = do
-    let deck = [(x, y) | x <- [1..count], y <- [1..count]]
-    let res = filter (\x -> length x >= count) (find1 [] deck)
     print res
     print (length res)
