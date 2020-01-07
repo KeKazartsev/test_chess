3ec002b2fcfa7d25459d1e8e925b1d7a9d0d84d5
Konstantin Kazartsev <kekazartsev@gmail.com>
2020-01-07 20:57:59 +0300

    haskell : simplify : change carried-lambda to 2-argumet function
    Signed-off-by: Konstantin Kazartsev <kekazartsev@gmail.com>

--- Main.hs
+++ Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
-test_hitten :: (Num(a), Ord(a)) => (a, a) -> ((a, a) -> Bool)
-test_hitten i = \ x -> (fst i /= fst x) && (snd i /= snd x) && (fst i - fst x /= snd i - snd x) && (fst i - fst x /= snd x - snd i)
+test_hitten :: (Num(a), Ord(a)) => (a, a) -> (a, a) -> Bool
+test_hitten i x = (fst i /= fst x) && (snd i /= snd x) && (fst i - fst x /= snd i - snd x) && (fst i - fst x /= snd x - snd i)
 
 find1 :: (Ord(a), Num(a)) => [(a, a)] -> [(a, a)] -> [[(a, a)]]
 find1 res_start [] = [res_start]
@@ -13,9 +13,6 @@ count = 11
 main :: IO ()
 main = do
     let deck = [(x, y) | x <- [1..count], y <- [1..count]]
-    let deck1 = filter (test_hitten (head deck)) deck
     let res = filter (\x -> length x >= count) (find1 [] deck)
     print res
     print (length res)
-    
-    
